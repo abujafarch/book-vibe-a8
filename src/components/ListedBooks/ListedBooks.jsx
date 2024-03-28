37d52452a29f04766e98ebddf5ba92df4240c8aa
AbujafarCh <357jafar@gmail.com>
2024-03-28 19:34:55 +0600

    sort by btn added

--- src/components/ListedBooks/ListedBooks.jsx
+++ src/components/ListedBooks/ListedBooks.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Tabs from "./Tabs";
+import SortBy from "./SortBy";
 
 const ListedBooks = () => {
     return (
         <div>
             <h1 className="text-center font-bold text-[28px] text-[#131313] bg-[#1313130D] px-3 py-8 rounded-2xl mb-8">Books</h1>
+            <SortBy></SortBy>
             <Tabs></Tabs>
             <div>
                 <Outlet></Outlet>
